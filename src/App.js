b211036f4f0d938998106444ba6abfe68b80eece
Leanne Sall <lsall1@my.bcit.ca>
2020-11-13 18:02:16 -0800

    added dummy songs, play button and stop and time

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react';
+import './styles/app.scss';
 import Player from './components/player';
 import Song from './components/song';
+import data from "./util";
+
 
 function App() {
+  const [songs, setSongs] = useState(data());
+  const [currentSong, setCurrentSong] = useState(songs[0]);
+  const [isPlaying, setIsPlaying] = useState(false);
   return (
     <div className="App">
       <h1>Skihist</h1>
-      <Song />
-      <Player />
+      <Song currentSong={currentSong} />
+      <Player
+        setIsPlaying={setIsPlaying}
+        isPlaying={isPlaying}
+        currentSong={currentSong} />
     </div>
   );
 }
